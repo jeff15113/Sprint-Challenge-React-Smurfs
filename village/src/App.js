e01f73dfabbf0006a709948e1c8b3108c82ad0b4
jeff15113 <itsmejeffery@gmail.com>
2019-01-25 13:44:44 -0500

    MVP complete

--- village/src/App.js
+++ village/src/App.js
@@ -1,21 +1,15 @@
 import React, { Component } from "react";
 import Axios from "axios";
+import { Route, NavLink } from "react-router-dom";
 import "./App.css";
 import SmurfForm from "./components/SmurfForm";
 import Smurfs from "./components/Smurfs";
-
-newSmurf = {
-  name: "",
-  height: "",
-  age: ""
-};
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
-      newSmurf: newSmurf
+      smurfs: []
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
@@ -29,35 +23,30 @@ class App extends Component {
       .catch(message => console.log(message));
   }
 
-  handleChanges = e => {
-    e.persist();
-    this.setState(prevState => {
-      return {
-        newSmurf: {
-          ...prevState.newSmurf,
-          [e.target.name]: e.target.value
-        }
-      };
-    });
-  };
-
-  addSmurf = e => {
-    e.preventDefault();
-    Axios.post(`http://localhost:3333/smurfs`, this.state.newSmurf)
-      .then(res => {
-        this.setState({ smurfs: res.data, newSmurf: newSmurf });
-      })
-      .catch(err => console.log(err));
+  getSmurfList = res => {
+    this.setState({ smurfs: res.data });
   };
 
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs
-          smurfs={this.state.smurfs}
-          handleChanges={this.handleChanges}
-          addSmurf={this.addSmurf}
+        <header className="headerbar">
+          <h1>**SMURF**</h1>
+          <div className="nav">
+            <NavLink to="/">Smurf List</NavLink>
+            <NavLink to="/smurf-form">Add Smurf</NavLink>
+          </div>
+        </header>
+        <Route
+          path="/smurf-form"
+          render={props => (
+            <SmurfForm {...props} getSmurfList={this.getSmurfList} />
+          )}
+        />
+        <Route
+          exact
+          path="/"
+          render={() => <Smurfs smurfs={this.state.smurfs} />}
         />
       </div>
     );
